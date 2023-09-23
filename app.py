e7fc42b9c0d7467251c6b585a340a5ec1fc7c8cd
Thanujan <k.thanujan.cs@gmail.com>
2023-09-23 10:54:09 -0700

    Major update, color palete added
    the project is almost complete no wall is left to fix and tweak some features and release it

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def pressedOnEraser(e):
 
 def add_value(e):
     global penwidth
-    if penwidth <= 29:
+    if penwidth <= 49:
         penwidth += 1
         numDisplay.configure(text = penwidth)
     else:
@@ -139,7 +139,7 @@ def sub_value(e):
         pass
 
 def save(e):
-    files = [('JPEG', '*.jpeg'),('PNG', '*.png')]
+    files = [('JPEG', '*.jpeg')]
     file = asksaveasfile(filetypes = files, defaultextension = files)
     myScreenshot = pyautogui.screenshot(region = (72,20 ,1650,1050))
 
@@ -183,80 +183,84 @@ def save_delay(e):
     root.after(2000,save(E))
 
 ## COLOR PALETS
-p1 = Button(frame,bg = '#ff1919',width = 4,height= 2,command=lambda:getColorCodeFromButton("#ff1919"))
-p2 = Button(frame,bg = '#ff7733',width = 4,height= 2,command=lambda:getColorCodeFromButton("#ff7733"))
-p3 = Button(frame,bg = '#ffbb34',width = 4,height= 2,command=lambda:getColorCodeFromButton("#ffbb34"))
-p4 = Button(frame,bg = '#ffff3e',width = 4,height= 2,command=lambda:getColorCodeFromButton("#ffff3e"))
-p5 = Button(frame,bg = '#bbfe33',width = 4,height= 2,command=lambda:getColorCodeFromButton("#bbfe33"))
+p1 = Button(frame,bg = '#ff0000',width = 4,height= 2,command=lambda:getColorCodeFromButton("#ff0000"))
+p2 = Button(frame,bg = '#ffff02',width = 4,height= 2,command=lambda:getColorCodeFromButton("#ffff02"))
+p3 = Button(frame,bg = '#00ff00',width = 4,height= 2,command=lambda:getColorCodeFromButton("#00ff00"))
+p4 = Button(frame,bg = '#00ffff',width = 4,height= 2,command=lambda:getColorCodeFromButton("#00ffff"))
+p5 = Button(frame,bg = '#0200ff',width = 4,height= 2,command=lambda:getColorCodeFromButton("##0200ff"))
 
-p6 = Button(frame,bg = '#bbfe33',width = 4,height= 2,command=lambda:getColorCodeFromButton("#bbfe33"))
-p7 = Button(frame,bg = '#66ff19',width = 4,height= 2,command=lambda:getColorCodeFromButton("#bbfe33"))
-p8 = Button(frame,bg = '#33ff77',width = 4,height= 2,command=lambda:getColorCodeFromButton("#bbfe33"))
-p9 = Button(frame,bg = '#4dfb88',width = 4,height= 2,command=lambda:getColorCodeFromButton("#3233ff"))
-p10 = Button(frame,bg = '#4dffc5',width = 4,height= 2,command=lambda:getColorCodeFromButton("#7734fe"))
+p6 = Button(frame,bg = '#800080',width = 4,height= 2,command=lambda:getColorCodeFromButton("#800080"))
+p7 = Button(frame,bg = '#FF6B6B',width = 4,height= 2,command=lambda:getColorCodeFromButton("#FF6B6B"))
+p8 = Button(frame,bg = '#FF00FF',width = 4,height= 2,command=lambda:getColorCodeFromButton("#FF00FF"))
+p9 = Button(frame,bg = '#A52A2A',width = 4,height= 2,command=lambda:getColorCodeFromButton("#A52A2A"))
+p10 = Button(frame,bg = '#FFC0CB',width = 4,height= 2,command=lambda:getColorCodeFromButton("#FFC0CB"))
 
-p11 = Button(frame,bg = '#33fdfd',width = 4,height= 2)
-p12 = Button(frame,bg = '#34baff',width = 4,height= 2)
-p13 = Button(frame,bg = '#3377fe',width = 4,height= 2)
-p14 = Button(frame,bg = '#3233ff',width = 4,height= 2)
-p15 = Button(frame,bg = 'red',width = 4,height= 2)
 
-p16 = Button(frame2,bg = '#ff7d92',width = 4,height= 2)
-p17 = Button(frame2,bg = '#fcb9c2',width = 4,height= 2)
-p18 = Button(frame2,bg = '#fed3b4',width = 4,height= 2)
-p19 = Button(frame2,bg = '#ffe5ce',width = 4,height= 2)
-p36 = Button(frame2,bg = '#faf8c9',width = 4,height= 2)
+p11 = Button(frame,bg = '#008080',width = 4,height= 2,command=lambda:getColorCodeFromButton("#008080"))
+p12 = Button(frame,bg = '#CCCCFF',width = 4,height= 2,command=lambda:getColorCodeFromButton("#CCCCFF"))
+p13 = Button(frame,bg = '#4B0082',width = 4,height= 2,command=lambda:getColorCodeFromButton("#4B0082"))
+p14 = Button(frame,bg = '#87CEEB',width = 4,height= 2,command=lambda:getColorCodeFromButton("#87CEEB"))
+p15 = Button(frame,bg = '#808000',width = 4,height= 2,command=lambda:getColorCodeFromButton("#808000"))
 
-p20 = Button(frame2,bg = 'black',width = 4,height= 2)
 
-p21 = Button(frame2,bg = '#f4f1e7',width = 4,height= 2)
-p22 = Button(frame2,bg = '#dcd7f4',width = 4,height= 2)
-p23 = Button(frame2,bg = '#c1bbdf',width = 4,height= 2)
-p24 = Button(frame2,bg = '#ccd0fe',width = 4,height= 2)
 
-p25 = Button(frame2,bg = '#f4f1e7',width = 4,height= 2)
+p16 = Button(frame2,bg = '#FFC0CB',width = 4,height= 2,command=lambda:getColorCodeFromButton("#FFC0CB"))
+p17 = Button(frame2,bg = '#AEC6CF',width = 4,height= 2,command=lambda:getColorCodeFromButton("#AEC6CF"))
+p18 = Button(frame2,bg = '#B0E57C',width = 4,height= 2,command=lambda:getColorCodeFromButton("#B0E57C"))
+p19 = Button(frame2,bg = '#FDFD96',width = 4,height= 2,command=lambda:getColorCodeFromButton("#FDFD96"))
+p36 = Button(frame2,bg = '#D7A9E3',width = 4,height= 2,command=lambda:getColorCodeFromButton("#D7A9E3"))
 
-p26 = Button(frame2,bg = 'black',width = 4,height= 2)
-p27 = Button(frame2,bg = 'black',width = 4,height= 2)
-p28 = Button(frame2,bg = 'black',width = 4,height= 2)
-p29 = Button(frame2,bg = 'black',width = 4,height= 2)
-p30 = Button(frame2,bg = 'black',width = 4,height= 2)
+p20 = Button(frame2,bg = '#D7A9E3',width = 4,height= 2,command=lambda:getColorCodeFromButton("#D7A9E3"))
 
-p31 = Button(frame3,bg = '#b99976',width = 4,height= 2)
-p32 = Button(frame3,bg = '#d2b48c',width = 4,height= 2)
-p33 = Button(frame3,bg = '#e5d3b3',width = 4,height= 2)
-p34 = Button(frame3,bg = '#987554',width = 4,height= 2)
-p35 = Button(frame3,bg = '#664229',width = 4,height= 2)
+p21 = Button(frame2,bg = '#FFB347',width = 4,height= 2,command=lambda:getColorCodeFromButton("#FFB347"))
+p22 = Button(frame2,bg = '#FF6B6B',width = 4,height= 2,command=lambda:getColorCodeFromButton("#FF6B6B"))
+p23 = Button(frame2,bg = '#98FB98',width = 4,height= 2,command=lambda:getColorCodeFromButton("#98FB98"))
+p24 = Button(frame2,bg = '#E6E6FA',width = 4,height= 2,command=lambda:getColorCodeFromButton("#E6E6FA"))
 
-p37 = Button(frame3,bg = '#8ba88e',width = 4,height= 2)
-p38= Button(frame3,bg = '#5a786f',width = 4,height= 2)
-p39 = Button(frame3,bg = '#3a4e51',width = 4,height= 2)
-p40 = Button(frame3,bg = '#323e45',width = 4,height= 2)
-p41 = Button(frame3,bg = '#323e45',width = 4,height= 2)
+p25 = Button(frame2,bg = '#FFDAB9',width = 4,height= 2,command=lambda:getColorCodeFromButton("#FFDAB9"))
 
-p42 = Button(frame3,bg = 'orange',width = 4,height= 2)
-p43 =  Button(frame3,bg = 'orange',width = 4,height= 2)
-p44 = Button(frame3,bg = 'orange',width = 4,height= 2)
-p45 = Button(frame3,bg = 'orange',width = 4,height= 2)
-p46 = Button(frame3,bg = 'orange',width = 4,height= 2)
+p26 = Button(frame2,bg = '#A1E7E6',width = 4,height= 2,command=lambda:getColorCodeFromButton("#A1E7E6"))
+p27 = Button(frame2,bg = '#C8A2C8',width = 4,height= 2,command=lambda:getColorCodeFromButton("#C8A2C8"))
+p28 = Button(frame2,bg = '#FE91A4',width = 4,height= 2,command=lambda:getColorCodeFromButton("#FE91A4"))
+p29 = Button(frame2,bg = '#C3CDE6',width = 4,height= 2,command=lambda:getColorCodeFromButton("#C3CDE6"))
+p30 = Button(frame2,bg = '#E2BE8A',width = 4,height= 2,command=lambda:getColorCodeFromButton("#E2BE8A"))
 
-p47 = Button(frame4,bg = 'red',width = 4,height= 2)
-p48 = Button(frame4,bg = 'red',width = 4,height= 2)
-p49 = Button(frame4,bg = 'red',width = 4,height= 2)
-p50 = Button(frame4,bg = 'red',width = 4,height= 2)
-p51 = Button(frame4,bg = 'red',width = 4,height= 2)
+p31 = Button(frame3,bg = '#e5d3b3',width = 4,height= 2,command=lambda:getColorCodeFromButton("#e5d3b3"))
+p32 = Button(frame3,bg = '#d2b48c',width = 4,height= 2,command=lambda:getColorCodeFromButton("#d2b48c"))
+p33 = Button(frame3,bg = '#b99976',width = 4,height= 2,command=lambda:getColorCodeFromButton("#b99976"))
+p34 = Button(frame3,bg = '#987554',width = 4,height= 2,command=lambda:getColorCodeFromButton("#987554"))
+p35 = Button(frame3,bg = '#664229',width = 4,height= 2,command=lambda:getColorCodeFromButton("#664229"))
 
-p52 = Button(frame4,bg = 'red',width = 4,height= 2)
-p53 = Button(frame4,bg = 'red',width = 4,height= 2)
-p54 = Button(frame4,bg = 'red',width = 4,height= 2)
-p55 = Button(frame4,bg = 'red',width = 4,height= 2)
-p56 = Button(frame4,bg = 'red',width = 4,height= 2)
+p37 = Button(frame3,bg = '#8ba88e',width = 4,height= 2,command=lambda:getColorCodeFromButton("#8ba88e"))
+p38= Button(frame3,bg = '#5a786f',width = 4,height= 2,command=lambda:getColorCodeFromButton("#5a786f"))
+p39 = Button(frame3,bg = '#3a4e51',width = 4,height= 2,command=lambda:getColorCodeFromButton("#3a4e51"))
+p40 = Button(frame3,bg = '#323e45',width = 4,height= 2,command=lambda:getColorCodeFromButton("#323e45"))
+p41 = Button(frame3,bg = '#323e45',width = 4,height= 2,command=lambda:getColorCodeFromButton("#323e45"))
 
-p57 = Button(frame4,bg = 'red',width = 4,height= 2)
-p58 = Button(frame4,bg = 'red',width = 4,height= 2)
-p59 = Button(frame4,bg = 'red',width = 4,height= 2)
-p60 = Button(frame4,bg = 'red',width = 4,height= 2)
-p61 = Button(frame4,bg = 'red',width = 4,height= 2)
+p42 = Button(frame3,bg = '#82b3dc',width = 4,height= 2,command=lambda:getColorCodeFromButton("#82b3dc"))
+p43 =  Button(frame3,bg = '#0077bb',width = 4,height= 2,command=lambda:getColorCodeFromButton("#0077bb"))
+p44 = Button(frame3,bg = '#0056a3',width = 4,height= 2,command=lambda:getColorCodeFromButton("#0056a3"))
+p45 = Button(frame3,bg = '#00448b',width = 4,height= 2,command=lambda:getColorCodeFromButton("#00448b"))
+p46 = Button(frame3,bg = '#012a60',width = 4,height= 2,command=lambda:getColorCodeFromButton("#012a60"))
+
+
+p47 = Button(frame4,bg = '#c8cccf',width = 4,height= 2,command=lambda:getColorCodeFromButton("##c8cccf"))
+p48 = Button(frame4,bg = '#b6babd',width = 4,height= 2,command=lambda:getColorCodeFromButton("#b6babd"))
+p49 = Button(frame4,bg = '#a7aaaf',width = 4,height= 2,command=lambda:getColorCodeFromButton("#a7aaaf"))
+p50 = Button(frame4,bg = '#989da3',width = 4,height= 2,command=lambda:getColorCodeFromButton("#989da3"))
+p51 = Button(frame4,bg = '#82878d',width = 4,height= 2,command=lambda:getColorCodeFromButton("#82878d"))
+
+p52 = Button(frame4,bg = '#64696d',width = 4,height= 2,command=lambda:getColorCodeFromButton("#64696d"))
+p53 = Button(frame4,bg = '#42454c',width = 4,height= 2,command=lambda:getColorCodeFromButton("#42454c"))
+p54 = Button(frame4,bg = '#363d45',width = 4,height= 2,command=lambda:getColorCodeFromButton("#363d45"))
+p55 = Button(frame4,bg = '#2b3036',width = 4,height= 2,command=lambda:getColorCodeFromButton("#2b3036"))
+p56 = Button(frame4,bg = '#242529',width = 4,height= 2,command=lambda:getColorCodeFromButton("#242529"))
+
+p57 = Button(frame4,bg = '#36393e',width = 4,height= 2,command=lambda:getColorCodeFromButton("#36393e"))
+p58 = Button(frame4,bg = '#20252b',width = 4,height= 2,command=lambda:getColorCodeFromButton("#20252b"))
+p59 = Button(frame4,bg = '#20252b',width = 4,height= 2,command=lambda:getColorCodeFromButton("#20252b"))
+p60 = Button(frame4,bg = '#17181d',width = 4,height= 2,command=lambda:getColorCodeFromButton("#17181d"))
+p61 = Button(frame4,bg = '#0c0d10',width = 4,height= 2,command=lambda:getColorCodeFromButton("#0c0d10"))
 
 ############## WIDGET GRIDDING #####################
 
@@ -270,13 +274,17 @@ button7.grid(row=41,column = 1,rowspan = 15,columnspan = 1,padx = 0)
 button8.grid(row=50,column = 1,rowspan = 1,columnspan = 1,padx = 11)
 
 subButton = Label(image =subButtonImage,bd=0,bg = "#161616")
-numDisplay = Label(text = f"{penwidth}",fg = '#c8c8c8',bd = 5,bg ="#161616",font = ("Hack",14) )
+numDisplay = Label(text = f"{penwidth}",fg = '#c8c8c8',bd = 5,bg ="#161616",font = ("Hack",12) )
 addButton = Label(image =addbuttonImage,bd=0,bg = "#161616")
 
-frame.grid(row=1,column = 100,rowspan =3 ,columnspan = 5,padx = 12)
-frame2.grid(row=2,column = 100,rowspan =10 ,columnspan = 5,padx = 12)
-frame3.grid(row=3,column = 100,rowspan =25 ,columnspan = 5,padx = 12)
-frame4.grid(row=4,column = 100,rowspan =40 ,columnspan = 5,padx = 12)
+frame.grid(row=1,column = 100,rowspan =3 ,columnspan = 4,padx = 9)
+frame2.grid(row=2,column = 100,rowspan =9 ,columnspan = 5)
+frame3.grid(row=3,column = 100,rowspan =24 ,columnspan = 5)
+frame4.grid(row=4,column = 100,rowspan =39 ,columnspan = 5)
+
+subButton.grid(row=5,column = 101,rowspan =90 ,columnspan = 3)
+numDisplay.grid(row=5,column = 102,rowspan =90 ,columnspan = 3)
+addButton.grid(row=5,column = 103,rowspan =90 ,columnspan = 3)
 
 p1.grid(row = 1,column = 1,rowspan = 1,columnspan = 1)
 p2.grid(row = 1,column = 2,rowspan = 1,columnspan = 1)
@@ -391,5 +399,12 @@ editMenu.add_command(label="Line Tool",command=lambda:pressedOnLineBrush(E))
 editMenu.add_command(label="Cascade Tool")
 editMenu.add_command(label="Eraser",command=lambda:pressedOnEraser(E))
 editMenu.add_command(label="Change pen color",command=colourChose)
+
+creditsBar = Menu(menubar, tearoff=0, font=("Hack", 13))
+
+menubar.add_cascade(label="Credits", menu=creditsBar)
+creditsBar.add_separator()
+creditsBar.add_command(label="Developer")
+
 
 root.mainloop()
